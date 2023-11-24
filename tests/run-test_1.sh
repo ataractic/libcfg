dec174e1ad95b7717d5effa8ee80a0ba31909532
mystère <contact@myst.re>
2023-11-24 20:37:48 +0100

    Update run-test_1.sh

--- tests/run-test_1.sh
+++ tests/run-test_1.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-gcc -std=c2x test_1.c ../src/cfg.c -o test_1.out && ./test_1.out
+clang -std=c2x -Wall -Wextra test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -g -O0 -fsanitize=address test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -g -O0 -fsanitize=memory test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -fsanitize=thread test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -fsanitize=undefined test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -fsanitize=cfi -flto -fvisibility=hidden test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -fsanitize=kcfi test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+clang -std=c2x -Wall -Wextra -fsanitize=safe-stack test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
+# clang -std=c2x -Wall -Wextra -fsanitize=dataflow test_1.c ../src/cfg.c -o test_1.out && ./test_1.out ${1}
